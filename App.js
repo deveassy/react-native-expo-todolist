f59a6cbaae2b4cdfcd15e16231edc8f9077d9811
deveassy <xjfmwld@gmail.com>
2020-08-03 23:42:10 +0900

    체크기능 styled-components 적용

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, ScrollView } from "react-native";
 import styled from "styled-components";
 
 import Header from "./components/Header";
--- components/Items.js
+++ components/Items.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// import { StyleSheet, TouchableOpacity } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import DeleteButton from "./DeleteButton";
@@ -10,16 +9,14 @@ export default function Items({ title, done, remove, toggle }) {
     <Swipeable renderRightActions={() => <DeleteButton onPress={remove} />}>
       <Container>
         <Item>
-          <CheckButton
-            activeOpacity={0.8}
-            onPress={toggle}
-            style={done ? check : uncheck}
-          >
-            <FontAwesome
-              name="check"
-              color={done ? "#000" : "#999"}
-              size={14}
-            />
+          <CheckButton activeOpacity={0.8} onPress={toggle}>
+            {done ? (
+              <Done>
+                <FontAwesome name="check" color="#000" size={14} />
+              </Done>
+            ) : (
+              <HaveTo />
+            )}
           </CheckButton>
           <ItemText>{title}</ItemText>
         </Item>
@@ -27,30 +24,6 @@ export default function Items({ title, done, remove, toggle }) {
     </Swipeable>
   );
 }
-
-// const styles = StyleSheet.create({
-//   done: {
-//     alignItems: "center",
-//     justifyContent: "center",
-//     width: 28,
-//     height: 28,
-//     marginRight: 10,
-//     backgroundColor: "#f3c623",
-//     borderRadius: 14,
-//     borderWidth: 1,
-//     borderColor: "transparent",
-//   },
-//   haveto: {
-//     alignItems: "center",
-//     justifyContent: "center",
-//     width: 28,
-//     height: 28,
-//     marginRight: 10,
-//     borderWidth: 1,
-//     borderColor: "#999",
-//     borderRadius: 14,
-//   },
-// });
 
 const Container = styled.View`
   padding: 0 25px;
@@ -75,12 +48,26 @@ const CheckButton = styled.TouchableOpacity`
   width: 28px;
   height: 28px;
   margin-right: 10px;
-  border: ${(style) => {
-    if (style.check) return "1px solid transparent";
-    if (style.uncheck) return "1px soild #999";
-  }};
   border-radius: 14px;
-  background-color: ${(style) => {
-    if (style.check) return "#f3c623";
-  }};
+`;
+const Done = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
+  margin-right: 10px;
+  border: 1px solid transparent;
+  border-radius: 14px;
+  background-color: #f3c623;
+`;
+const HaveTo = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
+  margin-right: 10px;
+  border: 2px solid #999;
+  border-radius: 14px;
 `;
